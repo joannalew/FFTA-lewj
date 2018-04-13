a667304e295891a93f8d12b34996a2b83531aed8
joannalew <lewj@oregonstate.edu>
2018-04-12 23:06:41 -0700

    add tutorial files

---
+++ FFTA-tutorial/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance;
+
+    public List<Tile> map = new List<Tile>();
+    private GameObject mapObject;
+
+    private Tile prevTile = null;
+    private Tile currTile = null;
+    private GameObject cursor;
+    private SpriteRenderer cursorSprite;
+
+	// Use this for initialization
+	private void Awake ()
+    {
+        Instance = this;
+        mapObject = transform.Find("giza1").gameObject;
+	}
+	
+    private void Start()
+    {
+        XMLManager.LoadMap(map, mapObject);
+        currTile = map[0];
+        cursor = (GameObject)Instantiate(PrefabHolder.Instance.Cursor, currTile.transform.position, Quaternion.identity);
+        cursorSprite = cursor.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Controls();
+	}
+
+    private void moveCursor(int direction)
+    {
+        prevTile = currTile;
+        currTile = currTile.neighbors[direction];
+        if (currTile != null)
+        {
+            cursor.transform.position = currTile.transform.position;
+            /*
+            if (currTile.hasObj)
+                cursorSprite.sortingLayerName = "3";
+            else
+                cursorSprite.sortingLayerName = "1";
+            */
+        }
+        else
+            currTile = prevTile;
+    }
+
+    public void Controls()
+    { 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            moveCursor(0);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            moveCursor(1);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            moveCursor(2);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            moveCursor(3);
+    }
+
+
+}
---
+++ FFTA-tutorial/Assets/Scripts/XMLManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml.Linq;
+
+
+public static class XMLManager
+{
+    public static void LoadMap(List<Tile> map, GameObject mapObject)
+    {
+        XDocument xmlDoc = XDocument.Load("Assets/Resources/giza1.xml");
+        IEnumerable<XElement> tiles = xmlDoc.Descendants("tile");
+
+        foreach (var tile in tiles)
+        {
+            Tile myTile = ((GameObject)Transform.Instantiate(PrefabHolder.Instance.Tile)).GetComponent<Tile>();
+
+            myTile.id = int.Parse(tile.Attribute("id").Value);
+            myTile.height = int.Parse(tile.Element("height").Value);
+            myTile.row = int.Parse(tile.Element("row").Value);
+            myTile.col = int.Parse(tile.Element("column").Value);
+            myTile.hasObj = int.Parse(tile.Element("object").Value) != 0;
+            myTile.neighbors = new Tile[4] { null, null, null, null };
+
+            float x = (myTile.row + myTile.col) * 1.0f;
+            float y = (myTile.row + myTile.height - myTile.col) * 0.5f;
+            float z = (myTile.row * 1.0f) - (myTile.col * 0.1f);
+            myTile.transform.position = new Vector3(x, y, z);
+            myTile.transform.parent = mapObject.transform;
+
+            map.Add(myTile);
+        }
+
+        foreach (var tile in tiles)
+        {
+            int id = int.Parse(tile.Attribute("id").Value);
+
+            if (int.Parse(tile.Element("left").Value) != -9)
+                map[id].neighbors[0] = map[int.Parse(tile.Element("left").Value)];
+
+            if (int.Parse(tile.Element("up").Value) != -9)
+                map[id].neighbors[1] = map[int.Parse(tile.Element("up").Value)];
+
+            if (int.Parse(tile.Element("right").Value) != -9)
+                map[id].neighbors[2] = map[int.Parse(tile.Element("right").Value)];
+
+            if (int.Parse(tile.Element("down").Value) != -9)
+                map[id].neighbors[3] = map[int.Parse(tile.Element("down").Value)]; 
+        }
+    }
+}
